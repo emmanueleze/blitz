3c27f82d117777489756ab063329aae196f3ec49
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2021-06-24 15:41:08 +0100

    struct AccumulatorTraits

--- alpha/include/alpha/core/policy.hpp
+++ alpha/include/alpha/core/policy.hpp
@@ -28,135 +28,160 @@
 
 namespace blitz {
 
-  namespace alg {
-    template<typename _Tp>
-    class Node;
-  }
+namespace alg {
+template <typename _Tp>
+class Node;
+}
 
-  template <bool>
-  struct CompileTimeError;
-  template <>
-  struct CompileTimeError<true> {};
+template <typename T>
+struct AccumulatorTraits;
+
+template <>
+struct AccumulatorTraits<char> {
+  using AccT = int;
+  static AccT const zero = 0;
+};
+
+template <>
+struct AccumulatorTraits<short> {
+  using AccT = int;
+  static AccT const zero = 0;
+};
+
+template <>
+struct AccumulatorTraits<int> {
+  using AccT = long;
+  static AccT const zero = 0;
+};
+
+template <>
+struct AccumulatorTraits<unsigned int> {
+  using AccT = unsigned long;
+  static AccT const zero = 0;
+};
+template <>
+struct AccumulatorTraits<float> {
+  using AccT = double;
+  static AccT const zero = 0;
+};
+
+template <bool>
+struct CompileTimeError;
+template <>
+struct CompileTimeError<true> {};
 
 #define STATIC_CHECK(expr) (CompileTimeError<(expr) != 0>())
 
-  template <typename _To, typename _From>
-  _To safe_reinterpret_cast(_From from) {
-    STATIC_CHECK(sizeof(_From) <= sizeof(_To));
+template <typename _To, typename _From>
+_To safe_reinterpret_cast(_From from) {
+  STATIC_CHECK(sizeof(_From) <= sizeof(_To));
 
-    return reinterpret_cast<_To>(from);
+  return reinterpret_cast<_To>(from);
+}
+
+template <typename T>
+class NewCreator {
+ public:
+  static T *Create() { return new T; }
+};
+
+template <typename T>
+class MallocCreator {
+ public:
+  static T *Create() {
+    void *buf = std::malloc(sizeof(T));
+    if (!buf) return 0;
+    return new (buf) T;
   }
+};
 
-  template <typename T>
-  class NewCreator {
-  public:
-    static T* Create() { return new T; }
-  };
+template <typename T>
+class PrototypeCreator {
+ public:
+  PrototypeCreator(T *p_obj = 0) : _prototype(p_obj) {}
+  T *Create() { return _prototype ? _prototype->Clone : 0; }
+  T *GetPrototype() { return _prototype; }
+  void SetPrototype(T *p_obj) { _prototype = p_obj; }
 
-  template <typename T>
-  class MallocCreator {
-  public:
-    static T* Create() {
-      void* buf = std::malloc(sizeof(T));
-      if (!buf) return 0;
-      return new (buf) T;
-    }
-  };
+ private:
+  T *_prototype;
+};
 
-  template <typename T>
-  class PrototypeCreator {
-  public:
-    PrototypeCreator(T* p_obj = 0) : _prototype(p_obj) {}
-    T* Create() { return _prototype ? _prototype->Clone : 0; }
-    T* GetPrototype() { return _prototype; }
-    void SetPrototype(T* p_obj) { _prototype = p_obj; }
+class Widget {
+ public:
+  Widget() { std::cout << "Widget::Created.\n"; }
+};
 
-  private:
-    T* _prototype;
-  };
+template <template <typename> class CreationPolicy>
+class WidgetManager : CreationPolicy<Widget> {
+ public:
+  WidgetManager() {}
+};
 
-  class Widget {
-  public:
-    Widget() { std::cout << "Widget::Created.\n"; }
-  };
+typedef WidgetManager<NewCreator> NewWidgetMgr;
+typedef WidgetManager<MallocCreator> MalWidgetMgr;
+typedef WidgetManager<PrototypeCreator> ProWidgetMgr;
 
-  template <template <typename> class CreationPolicy>
-  class WidgetManager : CreationPolicy<Widget> {
-  public:
-    WidgetManager() {}
-  };
-
-  typedef WidgetManager<NewCreator> NewWidgetMgr;
-  typedef WidgetManager<MallocCreator> MalWidgetMgr;
-  typedef WidgetManager<PrototypeCreator> ProWidgetMgr;
-
-  class Singleton {
-  public:
-    static Singleton* Instance() {
-      if (!_instance) {
-        if (_destroyed) {
-          OnDeadReference();
-        }
-        else {
-          Create();
-        }
+class Singleton {
+ public:
+  static Singleton *Instance() {
+    if (!_instance) {
+      if (_destroyed) {
+        OnDeadReference();
+      } else {
+        Create();
       }
-      return _instance;
     }
-    static void KillPhoenixSingleton();
-    void DisplayStatus() { std::clog << "Singleton Activated.\n"; }
+    return _instance;
+  }
+  static void KillPhoenixSingleton();
+  void DisplayStatus() { std::clog << "Singleton Activated.\n"; }
 
-  private:
-    static Singleton* _instance;
-    static bool _destroyed;
-    Singleton();
-    Singleton(const Singleton&);
-    Singleton& operator=(const Singleton&);
+ private:
+  static Singleton *_instance;
+  static bool _destroyed;
+  Singleton();
+  Singleton(const Singleton &);
+  Singleton &operator=(const Singleton &);
 
-  private:
-    static void Create() {
-      static Singleton* theInstance;
-      _instance = theInstance;
-    }
-    static void OnDeadReference();
-    virtual ~Singleton() {
-      _instance = 0;
-      _destroyed = true;
-      std::clog << "Destroyed.\n";
-    }
-  };
+ private:
+  static void Create() {
+    static Singleton *theInstance;
+    _instance = theInstance;
+  }
+  static void OnDeadReference();
+  virtual ~Singleton() {
+    _instance = 0;
+    _destroyed = true;
+    std::clog << "Destroyed.\n";
+  }
+};
 
-  enum NodeType {
-    NODE_SINGLE,
-    NODE_DOUBLE,
-    NODE_CIRC
-  };
+enum NodeType { NODE_SINGLE, NODE_DOUBLE, NODE_CIRC };
 
-  template <typename _Tp, typename NodeTp = alg::Node<_Tp> >
-  class SingleNode : public NodeTp {
-    public:
-      SingleNode(){}
+template <typename _Tp, typename NodeTp = alg::Node<_Tp>>
+class SingleNode : public NodeTp {
+ public:
+  SingleNode() {}
 
-      SingleNode* next;
-  };
+  SingleNode *next;
+};
 
-  template <typename _Tp, typename NodeTp = alg::Node<_Tp>>
-  class DoubleNode : public NodeTp{
-  public:
-    DoubleNode() {}
+template <typename _Tp, typename NodeTp = alg::Node<_Tp>>
+class DoubleNode : public NodeTp {
+ public:
+  DoubleNode() {}
 
-    
+  DoubleNode *prev;
+  DoubleNode *next;
+};
 
-    DoubleNode *prev;
-    DoubleNode *next;
-  };
+template <typename _Tp>
+class CircularNode {};
 
-  template <typename _Tp>
-  class CircularNode {};
-
-  template <typename T>
-  using EnableIfSingle =
-    std::enable_if_t<std::is_convertible_v<T, blitz::SingleNode<T>>>;
+template <typename T>
+using EnableIfSingle =
+  std::enable_if_t<std::is_convertible_v<T, blitz::SingleNode<T>>>;
 
 }  // namespace blitz
 
